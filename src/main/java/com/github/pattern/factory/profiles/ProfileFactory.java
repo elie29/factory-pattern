54fec6c25cff6772285fe3c0c0b87f323c79b6af
Elie NEHME <elie.nehme@soprasterianext.com>
2022-04-13 11:16:43 +0200

    🔨 Refactoring with hashMap

--- src/main/java/com/github/pattern/factory/profiles/ProfileFactory.java
+++ src/main/java/com/github/pattern/factory/profiles/ProfileFactory.java
@@ -1,5 +1,9 @@
 package com.github.pattern.factory.profiles;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 import static com.github.pattern.factory.profiles.Admin.ADMIN;
 import static com.github.pattern.factory.profiles.Hr.HR;
 import static com.github.pattern.factory.profiles.Hrd.HRD;
@@ -7,19 +11,20 @@ import static com.github.pattern.factory.profiles.Validator.VALIDATOR;
 
 public class ProfileFactory {
 
+   private final static Map<String, Supplier<Profile>> profiles = new HashMap<>();
+
+   static {
+      profiles.put(ADMIN, Admin::new);
+      profiles.put(HR, Hr::new);
+      profiles.put(HRD, Hrd::new);
+      profiles.put(VALIDATOR, Validator::new);
+   }
+
    public static Profile create(String profile) {
-      switch (profile) {
-      case ADMIN:
-         return new Admin();
-      case HR:
-         return new Hr();
-      case HRD:
-         return new Hrd();
-      case VALIDATOR:
-         return new Validator();
-      default:
-         return new Anonymous();
+      if (profiles.containsKey(profile)) {
+         return profiles.get(profile).get();
       }
+      return new Anonymous();
    }
 
 }
